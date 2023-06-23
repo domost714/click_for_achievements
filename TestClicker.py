646d74c1d2cc286f6ba53b9ffb3394892bac5403
domost714 <dominik.ostrzechowski@interia.pl>
2023-06-23 15:07:37 +0200

    add achievements label test

--- TestClicker.py
+++ TestClicker.py
@@ -12,6 +12,10 @@ class TestClicker:
         for i in range(times):
             clicker.button.invoke()
             i += 1
+        if times < 10:
+            assert str(bool(clicker.achievements.grid_info())) == "False"
+        else:
+            assert str(bool(clicker.achievements.grid_info())) == "True"
         root.after(1000, lambda: root.destroy())
         root.mainloop()
         assert clicker.clicks == times
@@ -28,3 +32,6 @@ class TestClicker:
 
     def test_several_clicks_counting(self):
         TestClicker.verify_button_after_clicking(5)
+
+    def test_achievements_label_unlocking(self):
+        TestClicker.verify_button_after_clicking(10)
