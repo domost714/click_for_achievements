f9b1f5e2d5c298cb8b9d2e041ead4621c0921ccf
domost714 <dominik.ostrzechowski@interia.pl>
2023-06-23 10:06:40 +0200

    tests reformat

--- TestClicker.py
+++ TestClicker.py
@@ -1,25 +1,28 @@
 from tkinter import *
+
 from Clicker import Clicker
 
 
 class TestClicker:
+    def create_the_frame():
+        root = Tk()
+        root.title("Click for achievements!")
+        root.geometry("300x50")
+        return root
+
     def test_initialisation(self):
         clicker = Clicker
         assert clicker
 
     def test_initial_counter(self):
-        root = Tk()
-        root.title("Click for achievements!")
-        root.geometry("300x50")
+        root = TestClicker.create_the_frame()
         clicker = Clicker(root)
         root.after(1000, lambda: root.destroy())
         root.mainloop()
         assert clicker.clicks == 0
 
     def test_first_click_counting(self):
-        root = Tk()
-        root.title("Click for achievements!")
-        root.geometry("300x50")
+        root = TestClicker.create_the_frame()
         clicker = Clicker(root)
         clicker.button.invoke()
         root.after(1000, lambda: root.destroy())
@@ -27,13 +30,11 @@ class TestClicker:
         assert clicker.clicks == 1
 
     def test_several_clicks_counting(self):
-        root = Tk()
-        root.title("Click for achievements!")
-        root.geometry("300x50")
+        root = TestClicker.create_the_frame()
         clicker = Clicker(root)
         for i in range(5):
             clicker.button.invoke()
-            i+= 1
+            i += 1
         root.after(1000, lambda: root.destroy())
         root.mainloop()
         assert clicker.clicks == 5
